56e1738847460b54b72f43b15924617fe2bdfa98
apple <apple@appledeMacBook-Pro.local>
2019-01-19 14:51:17 +0800

    project init commit

---
+++ src/main/java/com/zhangyi/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.zhangyi.dao;
+
+import com.zhangyi.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
